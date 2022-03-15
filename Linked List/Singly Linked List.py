314bd27f342b288ec4932d596942dd17f5111a09
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-03-15 21:57:03 +0530

    Update Singly Linked List.py

--- Linked List/Singly Linked List.py
+++ Linked List/Singly Linked List.py
@@ -20,3 +20,34 @@ class SinglyLinkedList:
             while n is not None:
                 print(n.data, end="-->")
                 n = n.ref
+                
+    # Step 4:
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.ref = self.head
+        self.head = new_node
+
+    # Step 5:
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.ref is not None:
+                n = n.ref
+            n.ref = new_node
+
+    # Step 6:
+    def insert_after(self, data, x):
+        n = self.head
+        while n is not None:
+            if n.data == x:
+                break
+            n = n.ref
+        if n is None:
+            print("Node is not present in a linked list!")
+        else:
+            new_node = Node(data)
+            new_node.ref = n.ref
+            n.ref = new_node
