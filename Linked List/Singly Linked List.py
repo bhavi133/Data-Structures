9090abefbc6c914b78ddf531f718448a33fa0dcf
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-03-16 19:31:53 +0530

    Update Singly Linked List.py

--- Linked List/Singly Linked List.py
+++ Linked List/Singly Linked List.py
@@ -51,3 +51,70 @@ class SinglyLinkedList:
             new_node = Node(data)
             new_node.ref = n.ref
             n.ref = new_node
+            
+    # Step 7:
+    def insert_before(self, data, x):
+        if self.head is None:
+            print("Linked list is empty!")
+        if self.head.data == x:
+            new_node = Node(data)
+            new_node.ref = self.head
+            self.head = new_node
+            return
+        n = self.head
+        while n.ref is not None:
+            if n.ref.data == x:
+                break
+            n = n.ref
+        if n.ref is None:
+            print("Node is not present in a linked list!")
+        else:
+            new_node = Node(data)
+            new_node.ref = n.ref
+            n.ref = new_node
+
+    # Step 8:
+    def delete_from_beginning(self):
+        if self.head is None:
+            print("Linked list is empty!")
+        else:
+            self.head = self.head.ref
+
+    # Step 9:
+    def delete_from_end(self):
+        if self.head is None:
+            print("Linked list is empty!")
+        else:
+            n = self.head
+            while n.ref.ref is not None:
+                n = n.ref
+            n.ref = None
+
+    # Step 10:
+    def delete_by_value(self, x):
+        if self.head is None:
+            print("Linked list is empty!")
+            return
+        if self.head.data == x:
+            self.head = self.head.ref
+        else:
+            n = self.head
+            while n.ref is not None:
+                if n.ref.data == x:
+                    break
+                n = n.ref
+            if n.ref is None:
+                print("Node is not present in a linked list!")
+            else:
+                n.ref = n.ref.ref
+
+
+ll1 = SinglyLinkedList()
+ll1.insert_at_beginning(10)
+ll1.insert_at_end(20)
+ll1.insert_after(30, 10)
+ll1.insert_before(40, 20)
+ll1.delete_from_beginning()
+ll1.delete_from_end()
+ll1.delete_by_value(30)
+ll1.traverse()
